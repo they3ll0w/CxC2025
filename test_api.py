bb6fddc688cc9accd84beeb2b57369dd8b6b1329
they3ll0w <lieric2004@hotmail.com>
2025-02-24 23:07:05 -0500

    REmoved test_api.py

--- test_api.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-params = {
-    'sequence': ['application-window-opened', 'session_start']
-}
-response = requests.post('https://localhost:5000/api/v1/predict', json=params)
-
-print(response.json())
